2b2e4cc4e4bdef839834f3d9bfcdb30394678ad0
Fred Kaloki <charlesfred285@gmail.com>
2025-08-21 21:49:54 +0300

    Added Plp folder

--- python-lessons/practice_file2.py
+++ python-lessons/practice_file2.py
@@ -17,24 +17,16 @@
 # In case of input data being supplied to the question, it should be assumed to be a console input.
 
 import re
-value = []
-items=[x for x in input().split(',')]
-for p in items:
-    if len(p)<6 or len(p)>12:
-        continue
-    else:
-        pass
-    if not re.search("[a-z]",p):
-        continue
-    elif not re.search("[0-9]",p):
-        continue
-    elif not re.search("[A-Z]",p):
-        continue
-    elif not re.search("[$#@]",p):
-        continue
-    elif re.search("s",p):
-        continue
-    else:
-        pass
-    value.append(p)
-print(",".join(value))
+
+passwords = input("Enter passwords separated by commas: ").split(',')
+valid_passwords = []
+
+for password in passwords:
+    if (6 <= len(password) <= 12 and
+        re.search("[a-z]", password) and       # at least one lowercase
+        re.search("[A-Z]", password) and       # at least one uppercase
+        re.search("[0-9]", password) and       # at least one digit
+        re.search("[$#@.]", password)):         # at least one special char
+        valid_passwords.append(password)
+
+print(",".join(valid_passwords))
